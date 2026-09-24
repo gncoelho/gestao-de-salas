842c306223b78d8aa372bd00f14d375a0ffc2c55
Guilherme Coelho <guincoelho@gmail.com>
2026-09-24 19:29:38 -0300

    login version

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
+    path('login/', LoginView.as_view(template_name='webapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('cadastro/', views.cadastro, name='cadastro'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from .forms import CadastroUsuarioForm
 
 
+@login_required
 def welcome(request):
     return render(request, 'webapp/welcome.html')
+
+
+def cadastro(request):
+    if request.user.is_authenticated:
+        return redirect('welcome')
+
+    if request.method == 'POST':
+        form = CadastroUsuarioForm(request.POST)
+        if form.is_valid():
+            usuario = form.save()
+            login(request, usuario)
+            return redirect('welcome')
+    else:
+        form = CadastroUsuarioForm()
+
+    return render(request, 'webapp/cadastro.html', {'form': form})
